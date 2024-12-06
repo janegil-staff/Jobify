d968d72f72817d77dd9d5e1dd8e40a6529010f88
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2024-12-06 10:02:09 +0100

    Server Auth Routers & Validation

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 import jobRouter from "./routers/jobRouter.js";
+import authRouter from "./routers/authRouter.js";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
+app.use(cookieParser());
 app.use(express.json());
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
