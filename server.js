a7e0bd08584aba7b4e537a9a5b287949b488aea1
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2024-12-06 18:48:33 +0100

    Update Profile && Upload Avatar to Cloudinary

--- server.js
+++ server.js
@@ -5,12 +5,24 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
+import cloudinary from "cloudinary";
 import cookieParser from "cookie-parser";
 import jobRouter from "./routers/jobRouter.js";
 import authRouter from "./routers/authRouter.js";
 import userRouter from "./routers/userRouter.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
-import { authenticateUser } from './middleware/authMiddleware.js';
+import { authenticateUser } from "./middleware/authMiddleware.js";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -18,10 +30,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+console.log(__dirname);
+app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.use('/api/v1/jobs', authenticateUser, jobRouter);
-app.use('/api/v1/users', authenticateUser, userRouter);
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
