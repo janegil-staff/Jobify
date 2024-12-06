e21d961f7e38e4e9d75a154f43b4b371c4ef48f0
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2024-12-06 12:00:13 +0100

    Proxy Setup && Custom Fetch to Api

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
-
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
