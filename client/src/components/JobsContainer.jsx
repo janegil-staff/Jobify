7d73da6b0aeb29f6ecf1384ee438ae83eb44dc71
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2024-12-07 20:17:28 +0100

    Pagination for All Joba

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,11 +1,11 @@
-import Job from "./Job";
-import Wrapper from "../assets/wrappers/JobsContainer";
-import { useAllJobsContext } from "../pages/AllJobs";
-
+import Job from './Job';
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useAllJobsContext } from '../pages/AllJobs';
+import PageBtnContainer from './PageBtnContainer';
 const JobsContainer = () => {
-  
   const { data } = useAllJobsContext();
-  const jobs = data.jobs;
+
+  const { jobs, totalJobs, numOfPages } = data;
   if (jobs.length === 0) {
     return (
       <Wrapper>
@@ -15,13 +15,16 @@ const JobsContainer = () => {
   }
   return (
     <Wrapper>
-      <div className="jobs">
+      <h5>
+        {totalJobs} job{jobs.length > 1 && 's'} found
+      </h5>
+      <div className='jobs'>
         {jobs.map((job) => {
           return <Job key={job._id} {...job} />;
         })}
       </div>
+      {numOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
-
 export default JobsContainer;
